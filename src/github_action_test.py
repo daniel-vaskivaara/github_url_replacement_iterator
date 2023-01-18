0f166e07dc51351bb257e5f3215b0d21c113363e
Daniel <daniel.vaskivaara@eficode.com>
2023-01-18 18:12:08 +0200

    fix: add __main__ test / function in test script

--- src/github_action_test.py
+++ src/github_action_test.py
@@ -1,6 +1,11 @@
 import sys
 
-print("printing args sent to github_action_test.py:")
-print(sys.argv[1:])
+# string to test search & replace Action -> find_me find_me find_me
 
-# find_me find_me find_me
+def main():
+    print("printing args sent to github_action_test.py:")
+    print(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
